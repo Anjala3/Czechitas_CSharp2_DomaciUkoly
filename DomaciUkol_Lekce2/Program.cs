6df145aa77f5630236baae4e4490bc4b73367b2d
Anna <komorova.an@gmail.com>
2024-05-01 13:17:47 +0200

    Vytvoreni Projektu

---
+++ DomaciUkol_Lekce2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DomaciUkol_Lekce2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
