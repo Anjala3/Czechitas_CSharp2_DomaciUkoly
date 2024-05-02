444ca2abcecee28572b5bfdca1d178ea65f728de
Anna <komorova.an@gmail.com>
2024-05-02 14:14:36 +0200

    Vytvoreni projektu

---
+++ CSharp2_DomaciUkol_Lekce3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp2_DomaciUkol_Lekce3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
