a84d4a16356d9b75ce3eff0877b881e14b691dfe
Anna <komorova.an@gmail.com>
2024-05-08 14:55:10 +0200

    Jak jsem stara

--- CSharp2_DomaciUkol_Lekce4/Program.cs
+++ CSharp2_DomaciUkol_Lekce4/Program.cs
@@ -7,7 +7,14 @@
             // 1. Vypiš aktuální datum a čas, nemusíš řešit, v kterém je to časovém pásmu.
 
             DateTime aktualniDatumACas = DateTime.Now;
-            Console.WriteLine(aktualniDatumACas.ToString());
+            Console.WriteLine($"Dnes je: {aktualniDatumACas}");
+
+            // 2. Vytvoř proměnnou typu DateTime a ulož do ní svoje datum narození. Potom vypiš, kolik dnů od té doby uteklo.
+
+            DateTime mojeDatumNarozeni = new DateTime(1989, 09, 20);
+            TimeSpan jakJsemStara = aktualniDatumACas - mojeDatumNarozeni;
+            Console.WriteLine($"Od meho narozeni ubehlo: {jakJsemStara.TotalDays} dnů.");
+
         }
     }
 }
